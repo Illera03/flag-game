2ecfe59f9992d1825501128cce5cc32e6781e1bf
Jorge <jillera10@gmail.com>
2024-12-06 00:37:42 +0100

    random options

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const button4 = document.getElementById('button4');
+const buttons = [button1, button2, button3, button4];
+let country;
+
 // Obtener bandera
 async function fetchFlag() {
     try {
@@ -8,16 +15,42 @@ async function fetchFlag() {
         const randomIndex = Math.floor(Math.random() * flags.length);
         const flag = flags[randomIndex];
 
-        console.log(flags); // Muestra las banderas
         const flagImg = document.getElementById('flag-img');
         if (flagImg) {
             flagImg.src = flag.flag;
+            console.log(flag.name);
+            country = flag.name;
         } else {
             console.error("Elemento 'flag-img' no encontrado en el DOM");
         }
     } catch (error) {
         console.error('Error obteniendo la bandera:', error);
     }
+}
+
+async function fetchOptions() {
+    console.log(country + " fetchOptions");
+    const random = Math.floor(Math.random() * 4);
+    const response = await fetch('/api/flags');
+    if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+
+    const countrys = await response.json();
+    let i = 0;
+    while (i < 4) {
+        const randomIndex = Math.floor(Math.random() * countrys.length);
+        countryOption = countrys[randomIndex].name;
+        if (random != i) {
+            if (countryOption != country) {
+                buttons[i].innerText = countryOption;
+                i++;
+            }
+        }
+        else {
+            buttons[random].innerText = country;
+            i++;
+        }
+    }
+
 }
 
 // Enviar puntuación
@@ -37,6 +70,11 @@ async function submitScore(userId, score) {
     }
 }
 
+// Obtener bandera y opciones
+async function fetchFlagAndOptions() {
+    await fetchFlag();
+    await fetchOptions();
+}
 document.addEventListener('DOMContentLoaded', () => {
-    fetchFlag();
+    fetchFlagAndOptions();
 });
